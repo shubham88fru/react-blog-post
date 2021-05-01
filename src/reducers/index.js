82c5c555814b3d97eff1adada489ecc5d61b0b4c
shubham88fru <shubham88fru@yahoo.com>
2021-05-01 18:52:26 +0530

    Complete posts reducer and display, add user reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import postReducer from './postsReducer';
 
 export default combineReducers({
-  replaceMe: () => 'hi',
+  posts: postReducer,
 });
---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,11 @@
+const postReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
+
+export default postReducer;
