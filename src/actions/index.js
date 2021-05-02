7d19e37b55d397a995a250514adb249303c3dc99
shubham88fru <shubham88fru@yahoo.com>
2021-05-02 19:14:25 +0530

    comment memoization using loadash and revert to old approach for fetchUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+//import _ from 'lodash';
 import jsonplaceholder from '../apis/jsonplaceholder';
 
 export const fetchPosts = () => async (dispatch) => {
@@ -6,11 +6,17 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => (dispatch) => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonplaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+/* USE LODASH MEMOIZATION */
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonplaceholder.get(`/users/${id}`);
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
